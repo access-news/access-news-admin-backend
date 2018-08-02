746a9dd47ab49b75e27a3d0fa0ad5295f05c50e0
Attila Gulyas <toraritte@gmail.com>
2018-08-02 16:05:58 -0700

    Low level store functions ready (?)

--- admin-functions.js
+++ admin-functions.js
@@ -31,15 +31,22 @@ function init_firebase_client() {
 
 const firebase_client = init_firebase_client();
 
+var FIREBASE_APP = firebase_admin;
+
 /* ADMIN COMMANDS */
 
 /* 0. Helpers */
 
-function store_stream_event(fire_app, fields, version, seq) {
+function create_stream() {
+    return FIREBASE_APP.database().ref("event_store").push().key;
+}
+
+function append_event_to_stream(stream_id, event_name, fields, version) {
 
     /* General structure of events:
 
-       event_store/stream_id/event_id/event/...fields.../timestamp/version/seq
+                                      ----- event object ------
+       event_store/stream_id/event_id/{event_name/...fields...},timestamp,version,seq
 
        stream_id: unique identifier of the aggregate instance (such as user_id,
                   category_id etc.). Basically an entity that should have its
@@ -51,118 +58,51 @@ function store_stream_event(fire_app, fields, version, seq) {
                   with a person's stream because they always have users, and
                   introducing a new group should have a purpose, and therefore
                   initial users.
+
+                  Ignored, when seq===0
+
         seq: "expected_version" in other event store implementations, but I
              think that name is misleading, especially if one tries to version
              their events. It is a sequential number for every event in the
              stream that, denoting chronological sequence.
 
-             Calling the function with seq=0 implies the start of a new stream.
+             Calling the function with seq===0 implies the start of a new stream.
     */
 
-    Object.assign(
-        fields,
-        {
-            "timestamp": fire_app.database.ServerValue.TIMESTAMP,
-            "version":   version,
-            "seq":       seq
-        }
-    );
+    /* DROPPING SEQ (may regret it soon after)
 
-    return fire_app.database().ref("event_store").push(fields);
+       I don't really know how to enqueue events headed to the store to enforce
+       order, but every event has a push ID (client side date) and a server
+       timestamp. These are not infallible though, so I will plan for best effort
+       for now.
+    */
+
+    var event = {
+        "event_name": event_name,
+        "fields": fields,
+        "timestamp": FIREBASE_APP.database.ServerValue.TIMESTAMP,
+        "version":   version
+    };
+
+    FIREBASE_APP.database().ref("event_store").child(stream_id).push(event);
 };
 
-/* 1. People */
+function start_new_stream_with_event(event_name, fields, version) {
 
-function add_person(name, fire_app) {
+    const id_of_new_stream = create_stream();
+    append_event_to_stream(id_of_new_stream, event_name, fields, version);
 
-    /* nameObject: {
-            first: "Bala",
-            last:  "Bab"
-        },
-
-       fire_app: Firebase app instance (admin, client, etc.)
-    */
-
-    /* Start a new PEOPLE stream, returning a Firebase Realtime DB
-       promise, that can be chained. */
-    return store_event(fire_app, nameObject, 0, 0);
-
+    return id_of_new_stream;
 }
-function add_user(person, account_type) {
 
-    /* `person` object:
-       ================
-        {
-            name: {
-                first: "Bala",
-                last:  "Bab"
-            },
-            email: "ema@il.com"
-        }
-
-        `account_type`: [ "admin" | "reader" | "listener" ]
-    */
-
-    firebase_admin.auth().createUser({ email: person.email }).then(function(userRecord) {
-
-        const db = firebase_admin.database();
-        const people_ref = db.ref("event_store");
-        const timestamp = firebase_admin.database.ServerValue.TIMESTAMP;
-
-        /* If user creation is successful, save "person_added" event, ... */
-
-        store_event(
-            people_ref,
-            "person_added",
-            {
-                "user_id": userRecord.uid,
-                "name": {
-                    "first": person.name.first,
-                    "last":  person.name.last
-                }
-            },
-            timestamp,
-            0
-
-        ).then(function(_ref) {
-
-            /* ... save "person_email_added" after above event finishes, and ... */
-
-            store_event(
-                people_ref,
-                "person_email_added",
-                {
-                    "user_id": userRecord.uid,
-                    "value":   person.email
-                },
-                timestamp,
-                0
-            )
-        }).then(function(_ref) {
-
-            /* ... finally store the "<account>_added" event. */
-
-            const account_event = account_type + "_added";
-
-            store_event(
-                db.ref("event_store"),
-                account_event,
-                {
-                    "user_id":  userRecord.uid,
-                    "username": person.email
-                },
-                timestamp,
-                0
-            );
-        }).catch(function(error) { console.log(error) });
-
-        firebase_client.auth().sendPasswordResetEmail(person.email);
-    });
-};
+var EVENT_VERSION = 0;
 
 module.exports = {
     firebase_admin,
     firebase_client,
-    store_event,
-    add_user
+    create_stream,
+    append_event_to_stream,
+    start_new_stream_with_event,
+    EVENT_VERSION,
+    FIREBASE_APP
 };
