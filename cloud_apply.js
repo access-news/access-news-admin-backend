23475e0924a67198a82f7caf7ee47feacb86ac94
Attila Gulyas <toraritte@gmail.com>
2018-09-10 14:08:47 -0700

    Rework cloud_apply() to exclude fetching previous state

--- cloud_apply.js
+++ cloud_apply.js
@@ -1,177 +1,153 @@
-function cloud_apply() {
+function cloud_apply(firebase_app) {
 
-  ADMIN_APP.database().ref("/event_store").on(
+  firebase_app.database().ref("/event_store").on(
     'child_added',
     function(event_snapshot) {
 
-      const event = event_snapshot.val();
-      const stream_id = event.stream_id;
+      function dispatch_event_handler() {
 
-      const state_ref = ADMIN_APP.database().ref("/state_store").child(stream_id);
+        const event = event_snapshot.val();
+        const event_id = event_snapshot.ref.getKey();
 
-      state_ref.once("value").then(
+        const stream_id = event.stream_id;
 
-        function(state_snapshot) {
+        const state_ref = firebase_app.database().ref("/state_store").child(stream_id);
 
-          /* Mutating the state, because `Object.assign()` only makes a shallow copy.
-          */
-          const state = state_snapshot.val();
-          const state_seq = (state === null) ? 0 : state.seq;
-
-          function check() {
-            const event_id = event_snapshot.ref.getKey();
-            const action = (event.seq < state_seq) ? "no op" : "replay";
-            console.log(`Stream: ${stream_id} event: ${event_id} ${event.event_name}`);
-            console.log(`  Action: ${action} (event_seq: ${event.seq}, state_seq: ${state_seq})`);
-            if (action === "replay") {
-              console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
-            }
-          }
-
-          if ( event.seq < state_seq ) {
-            check();
-            return;
-          } else {
-
-            check();
-
-            function make_event_handler() {
-
-              function sanitize_key(key) {
-                return key.split('').map(
-                  function(c) {
-                    switch (c) {
-                      case ".": return "<dot>";
-                      case "#": return "<hash>";
-                      case "$": return "<dollar>";
-                      case "/": return "<forward-slash>";
-                      case "[": return "<opening-bracket>";
-                      case "]": return "<closing-bracket>";
-                      default:  return c;
-                    }
-                  }).join('');
+        function sanitize_key(key) {
+          return key.split('').map(
+            function(c) {
+              switch (c) {
+                case ".": return "<dot>";
+                case "#": return "<hash>";
+                case "$": return "<dollar>";
+                case "/": return "<forward-slash>";
+                case "[": return "<opening-bracket>";
+                case "]": return "<closing-bracket>";
+                default:  return c;
               }
-
-              var update = {};
-              update["seq"] = event.seq;
-              update["aggregate"] = event.aggregate;
-
-              /* For list attributes of an aggregate, such as emails,
-                 phone numbers, etc.
-
-                 After dropping the "reason" and "from" fields (where included),
-                 there should only be one field remaining. (See commands above.)
-                 If this is not the case, I messed up. Again.
-              */
-              function for_multi(p) {
-
-                /* The "p" (as in "parameters") object in the factories below it
-                  therefore mostly to make pluralization rules explicit:
-
-                        "attribute": The plural name of the attribute in the state.
-
-                        "event_field": The singular event field name for the datum.
-                */
-
-                return function(event_snapshot, state) {
-
-                  const event =    event_snapshot.val();
-                  const event_id = event_snapshot.ref.getKey();
-
-                  delete event.fields.reason;
-
-                  if ( event.fields.from !== undefined) {
-
-                    update[`${p.attr}/${sanitize_key(event.fields.from)}`] = null;
-                    delete event.fields.from;
-                  }
-
-                  const key = Object.keys(event.fields)[0];
-                  const v   = event["fields"][key];
-
-                  update[`${p.attr}/${sanitize_key(v)}`] = !p.drop ? event_id : null;
-
-                  state_ref.update(update);
-                }
-              }
-
-              function for_person_name() {
-
-                return function(event_snapshot, state) {
-
-                  const event = event_snapshot.val();
-
-                  delete event.fields.reason
-                  update["name"] = event.fields;
-
-                  state_ref.update(update);
-                }
-              }
-
-              switch (event.event_name) {
-
-                case "person_added":
-                case "person_name_changed":
-                  return for_person_name();
-
-                case "email_added":
-                case "email_updated":
-                  return for_multi({
-                    attr: "emails",
-                  });
-                case "email_deleted":
-                  return for_multi({
-                    attr: "emails",
-                    drop: true
-                  });
-
-                case "phone_number_added":
-                case "phone_number_updated":
-                  return for_multi({
-                    attr: "phone_numbers",
-                  });
-                case "phone_number_deleted":
-                  return for_multi({
-                    attr: "phone_numbers",
-                    drop: true
-                  });
-
-                case "added_to_group":
-                  return for_multi({
-                    attr: "groups",
-                  });
-                case "removed_from_group":
-                  return for_multi({
-                    attr: "groups",
-                    drop: true
-                  });
-
-                // case "session_started":
-                // case "session_time_updated":
-                // case "session_ended":
-                //   return for_multi({
-                //     attr: "sessions",
-                //   });
-
-                // case "recording_added":
-                //   return for_multi({
-                //     attr: "recordings"
-                //   });
-
-                /* No `default` case. If event is not found,
-                   I messed something up.
-                */
-              }
-            }
-
-            const update_state = make_event_handler();
-
-            update_state(event_snapshot, state);
-
-          }
-
+            }).join('');
         }
-      );
+
+        function update_stream_state(update) {
+          update["_meta/seq"] = event.seq;
+          update["_meta/aggregate"] = event.aggregate;
+
+          update[`_meta/event_ids/${event_id}`] = event.timestamp;
+
+          state_ref.update(update);
+        }
+
+        /* For list attributes of an aggregate, such as emails,
+            phone numbers, etc.
+
+            After dropping the "reason" and "from" fields (where included),
+            there should only be one field remaining. (See commands above.)
+            If this is not the case, I messed up. Again.
+        */
+        function for_multi(p) {
+
+          /* The "p" (as in "parameters") object in the factories below it
+            therefore mostly to make pluralization rules explicit:
+
+                  "attribute": The plural name of the attribute in the state.
+
+                  "event_field": The singular event field name for the datum.
+          */
+
+          return function(event_snapshot) {
+
+            delete event.fields.reason;
+
+            var update = {};
+            if ( event.fields.from !== undefined) {
+
+              update[`${p.attr}/${sanitize_key(event.fields.from)}`] = null;
+              delete event.fields.from;
+            }
+
+            const k = Object.keys(event.fields)[0];
+            const v   = event["fields"][k];
+
+            update[`${p.attr}/${sanitize_key(v)}`] = !p.drop ? event_id : null;
+
+            update_stream_state(update);
+          }
+        }
+
+        function for_person_name() {
+
+          return function(event_snapshot) {
+
+            delete event.fields.reason
+
+            var update = {};
+            update["name"] = event.fields;
+
+            update_stream_state(update);
+          }
+        }
+
+        switch (event.event_name) {
+
+          case "person_added":
+          case "person_name_changed":
+            return for_person_name();
+
+          case "email_added":
+          case "email_updated":
+            return for_multi({
+              attr: "emails",
+            });
+          case "email_deleted":
+            return for_multi({
+              attr: "emails",
+              drop: true
+            });
+
+          case "phone_number_added":
+          case "phone_number_updated":
+            return for_multi({
+              attr: "phone_numbers",
+            });
+          case "phone_number_deleted":
+            return for_multi({
+              attr: "phone_numbers",
+              drop: true
+            });
+
+          case "added_to_group":
+            return for_multi({
+              attr: "groups",
+            });
+          case "removed_from_group":
+            return for_multi({
+              attr: "groups",
+              drop: true
+            });
+
+          // case "session_started":
+          // case "session_time_updated":
+          // case "session_ended":
+          //   return for_multi({
+          //     attr: "sessions",
+          //   });
+
+          // case "recording_added":
+          //   return for_multi({
+          //     attr: "recordings"
+          //   });
+
+          /* No `default` case. If event is not found,
+              I messed something up.
+          */
+        }
+      }
+
+      dispatch_event_handler()(event_snapshot);
     }
   );
 }
+
+module.exports = {
+  cloud_apply
+};
